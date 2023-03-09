e99a4e8e4865c9995c13562936e6794e71d1f601
aaira alias <aaaira.alias@gmail.com>
2023-03-10 01:11:57 +0800

    Add Text Mesh Pro GUI variable to Quiz script

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Quiz : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     void Start()
     {
