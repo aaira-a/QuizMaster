5599c522abe901d3a85e126cc7482cca1e737297
aaira alias <aaaira.alias@gmail.com>
2023-03-11 23:59:35 +0800

    Wire up Answer Buttons objects to Quiz component in QuizCanvas

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -7,6 +7,8 @@ public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
+
     void Start()
     {
         questionText.text = question.GetQuestion();
